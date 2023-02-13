1bb4102a7efdcbb8f38ba452b6100aa84202b3d4
nickglu86 <enayfls@gmail.com>
2023-02-13 17:27:41 +0200

    done

--- src/App.js
+++ src/App.js
@@ -1,37 +1,36 @@
-import React from 'react';
-import { CssBaseline, ThemeProvider } from '@mui/material';
-import { Routes, Route } from 'react-router-dom';
-import { ColorModeContext, useMode} from './theme';
-import TopBar from './views/global/TopBar';
-import Dashboard from './views/dashboard';
-import SideBar from './views/global/SideBar';
-// import Team from './views/team';
-// import Invoices from './views/invoices';
-// import Contact from './views/contact';
-// import Bar from './views/bar';
-// import Form from './views/form';
-// import Line from './views/line';
-// import Pie from './views/pie';
-// import FAQ from './views/faq';
-// import Geography from './views/geography';
-// import Calendar from './views/calendar';
-
-
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import Topbar from "./views/global/TopBar";
+import Sidebar from "./views/global/SideBar";
+import Dashboard from "./views/dashboard";
+import Team from "./views/team";
+import Invoices from "./views/invoices";
+import Contacts from "./views/contacts";
+import Bar from "./views/bar";
+import Form from "./views/form";
+import Line from "./views/line";
+import Pie from "./views/pie";
+import FAQ from "./views/faq";
+import Geography from "./views/geography";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { ColorModeContext, useMode } from "./theme";
+// import Calendar from "./views/calendar/calendar";
 
 function App() {
   const [theme, colorMode] = useMode();
+  const [isSidebar, setIsSidebar] = useState(true);
 
   return (
     <ColorModeContext.Provider value={colorMode}>
-      <ThemeProvider theme={theme} >
+      <ThemeProvider theme={theme}>
         <CssBaseline />
         <div className="app">
-        <SideBar /> 
+          <Sidebar isSidebar={isSidebar} />
           <main className="content">
-            <TopBar />
+            <Topbar setIsSidebar={setIsSidebar} />
             <Routes>
-              <Route path="/" element={<Dashboard />}></Route>
-              {/* <Route path="/team" element={<Team />} />
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/team" element={<Team />} />
               <Route path="/contacts" element={<Contacts />} />
               <Route path="/invoices" element={<Invoices />} />
               <Route path="/form" element={<Form />} />
@@ -39,8 +38,8 @@ function App() {
               <Route path="/pie" element={<Pie />} />
               <Route path="/line" element={<Line />} />
               <Route path="/faq" element={<FAQ />} />
-              <Route path="/calendar" element={<Calendar />} />
-              <Route path="/geography" element={<Geography />} /> */}
+              {/* <Route path="/calendar" element={<Calendar />} /> */}
+              <Route path="/geography" element={<Geography />} />
             </Routes>
           </main>
         </div>
